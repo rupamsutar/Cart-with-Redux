8420f1c9e5d1b0b668bc3e3282f461898aa6d090
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-15 20:37:48 +0530

    cart slice complete

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,33 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const DUMMY_DATA = [
+    {
+      id: 1,
+      price: 100,
+      title: "Do Epic Shit",
+      description: "Author: Ankur Warikoo"
+    },
+    {
+      id: 2,
+      price: 200,
+      title: "Deep Work",
+      description: "Author: Cal Newport"
+    }
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map((book) => (
+          <ProductItem
+            key={book.id}
+            id={book.id}
+            title= {book.title}
+            price={book.price}
+            description={book.description} 
+          />
+        ))}
       </ul>
     </section>
   );
