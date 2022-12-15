6e257471135166f863a3b5ab209c9089df2578b0
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-15 18:27:02 +0530

    remove cart added in cartSlice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -22,9 +22,22 @@ const cartSlice = createSlice({
                 existingItem.quantity++;
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+
+            if (existingItem.quantity === 1) {
+                state.items.filter((item) => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
         }
+
     }
 });
 
+export const cartActions = cartSlice.actions;
 export default cartSlice;
 
